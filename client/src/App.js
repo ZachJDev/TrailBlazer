e1ab01816cf88ef3366bff24e92f5d73f7611576
Zach Johnson <Zachjdev2121@gmail.com>
2020-11-02 07:05:59 -0500

    route planning

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,14 @@ function App() {
     <Router>
     <Switch>
   <Route path={"/home"} exact render={() =><Home/>}/>
+  {
+    // Login Page
+    // Park Page
+    // Trail Page
+    // User Page
+    // Add/Edit Park page
+    // Add/Edit Trail
+  }
     <Route path="/:b" render={() =><NotFound/>} />
     <Redirect path="/" exact to="/home"/>
     </Switch>
