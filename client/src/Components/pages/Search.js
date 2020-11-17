37fc717c74a467f94678e98c690008d70987d1c6
Zach Johnson <Zachjdev2121@gmail.com>
2020-11-17 07:22:48 -0500

    Search Page w/hooks

--- client/src/Components/pages/Search.js
+++ client/src/Components/pages/Search.js
@@ -1,62 +1,51 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import SearchResultContainer from "../SearchResultContainer";
+import FormInputText from "../FormInputText";
 
-export default class Search extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      searchType: "Park",
-      searchTerm: "",
-      searchResults: [],
-    };
-  }
-  handleSearchChange = (event) => {
-    event.preventDefault();
-    this.setState({ [event.target.name]: event.target.value });
-  };
-  handleSearch = () => {
-    fetch(`/search/${this.state.searchType}`)
+import useInputState from "../../hooks/useInputState";
+
+export default function Search() {
+  const [searchType, setSearchType] = useInputState("Park");
+  const [searchTerm, setSearchTerm] = useInputState("");
+  let [searchResults, setSearchResults] = useState([]);
+
+ const handleSearch = () => {
+   console.log(searchType, searchTerm)
+    fetch(`/search/${searchType}`)
       .then((res) => {
         return res.json();
       })
       .then((results) => {
-        this.setState({ searchResults: results });
+       setSearchResults(results);
       });
   };
-  render() {
-    return (
-      <div>
-        <p>Search page</p>
-        <div className="search-div">
-          <select name="searchType" onChange={this.handleSearchChange}>
-            <option value="Park">Park</option>
-            <option value="Trail">Trail</option>
-          </select>
-          <input
-            type="text"
-            name="searchTerm"
-            onChange={this.handleSearchChange}
-            value={this.state.searchTerm}
-          />
-          <button onClick={this.handleSearch}>submit!</button>
-        </div>
-        <section className="results">
-          {this.state.searchResults.length > 0
-            ? this.state.searchResults.map((park) => {
-                console.log(park);
-                return (
-                  <SearchResultContainer
-                    key={park.parkId}
-                    name={park.name}
-                    city={park.city}
-                    state={park.state}
-                    pictureUrl ={""}
-                  />
-                );
-              })
-            : "There's nothing to show!"}
-        </section>
+
+  return (
+    <div>
+      <p>Search page</p>
+      <div className="search-div">
+        <select name="searchType" onChange={setSearchType}>
+          <option value="Park">Park</option>
+          <option value="Trail">Trail</option>
+        </select>
+        <FormInputText value={searchTerm} handleChange={setSearchTerm} label="search" name="search" cssClass="search-input" />
+        <button onClick={handleSearch}>submit!</button>
       </div>
-    );
-  }
+      <section className="results">
+        {searchResults.length > 0
+          ? searchResults.map((park) => {
+              return (
+                <SearchResultContainer
+                  key={park.parkId}
+                  name={park.name}
+                  city={park.city}
+                  state={park.state}
+                  pictureUrl={""}
+                />
+              );
+            })
+          : "There's nothing to show!"}
+      </section>
+    </div>
+  );
 }
