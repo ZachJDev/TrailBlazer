b8a80221aefb59ac15ca5e6cf1761fd36c9bd4ae
Zach Johnson <Zachjdev2121@gmail.com>
2020-11-18 10:40:48 -0500

    decided again to make the parks page with a function.

--- client/src/Components/pages/Park.js
+++ client/src/Components/pages/Park.js
@@ -1,44 +1,18 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from "react";
+import useGetPayload from '../../hooks/useGetPayload'
 
+export default function Park({ match }) {
+  // I imagine that using something like this is better for React
+  // than just storing values in the function block.
+  const [parkInfo, resComplete, fetchFailed] = useGetPayload(`/park/${match.params.parkId}`)
 
-export default class Park extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            parkInfo: {},
-            parkLoaded: false,
-            fetchFailed: false
-        }
-    }
-
-    componentDidMount = () => {
-        let parkId = this.props.match.params.parkId
-        let status;
-        fetch(`/park/${parkId}`)
-        .then(res => {
-            status = res.status
-            if(res.status !== 200) throw new Error("bad Fetch")
-            else return res.json()})
-        .then(parkInfo => {
-            console.log(parkInfo)
-            this.setState({parkInfo, parkLoaded: true})
-        })
-        .catch(e => {
-            // I'll be creating a new 'notfound' component that will be told to render here
-            this.setState({fetchFailed: true})
-            console.log(status)
-            console.log(e)
-        })
-    }
-    render() {
-        let parkName = ''
-        if(this.state.fetchFailed) parkName = 'not Found'
-        else parkName = this.state.parkInfo.name
-        return (
-            <div>
-                <p>Park Page</p>
-                <p>{parkName}</p>
-            </div>
-        )
-    }
+  let parkName = "";
+  if (fetchFailed) parkName = "not Found";
+  else parkName = parkInfo?.name;
+  return (
+    <div>
+      <p>Park Page</p>
+      <p>{parkName}</p>
+    </div>
+  );
 }
