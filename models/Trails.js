e3122b86df9270d520bd3616b4f3fc3f37ace426
Zach Johnson <Zachjdev2121@gmail.com>
2020-11-24 07:32:48 -0500

    added some comments about how I plan to handle lengths.

--- models/Trails.js
+++ models/Trails.js
@@ -22,8 +22,9 @@ module.exports = (sequelize, Sequelize) => {
         },
         length: {
             // I'm going to need to double check MySQL floats, but I think this will be sufficient.
+            // I'm also thinking of just storing everything as miles, and then converting in the app at some point.
             type: DataTypes.FLOAT(5, 2)
-        }
+        },
     }, )
     return Trail
 }
