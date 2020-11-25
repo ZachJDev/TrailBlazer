1bd8fe8e5b03caeb289a3059dd9de3867a749773
Zach Johnson <Zachjdev2121@gmail.com>
2020-11-25 07:02:48 -0500

    trail payload request and basic display

--- client/src/Components/pages/Trail.js
+++ client/src/Components/pages/Trail.js
@@ -1,9 +1,16 @@
 import React from "react";
+import useGetPayload from '../../hooks/useGetPayload'
+import {Link} from 'react-router-dom'
 
-export default function Trail() {
+export default function Trail({match, history}) {
+    const [trailInfo] = useGetPayload(`/trail/${match.params.trailId}`)
+    //I'll need to handle any 404 errors here, I think.
+    const {length, name, description, parkId} = trailInfo
   return (
     <div>
-      <p>Trail</p>
+      <h1>{name}: {length} miles</h1>
+      <p>{description}</p>
+    <Link to={`/park/${parkId}`}>Go to Park</Link>
     </div>
   );
 }
