9597e1f498166a56f105318b6c5aa061d796b535
Zach Johnson <Zachjdev2121@gmail.com>
2020-12-01 06:41:45 -0500

    changed clear function in InputState hook to reset to initial state

--- client/src/hooks/useInputState.js
+++ client/src/hooks/useInputState.js
@@ -3,6 +3,6 @@ import {useState} from 'react'
 export default function useInputState (initState = "") {
     const [state, setState] = useState(initState);
     const setInput = (e) => setState(e.target.value);
-    const clear = () => setState('')
+    const clear = () => setState(initState)
     return [state, setInput, clear]
 }
