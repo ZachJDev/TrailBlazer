f92a0ab3111a0f50c5917b2a7125fa57b521a3f4
Zach Johnson <Zachjdev2121@gmail.com>
2020-12-02 06:42:16 -0500

    added "About" cards on Homepage

--- client/src/Components/pages/Home.js
+++ client/src/Components/pages/Home.js
@@ -6,8 +6,8 @@ import AboutCards from '../AboutCards'
 
 export default function Home() {
   return (
-    <Container fluid
-        style={{
+    <Container fluid>
+        <div style={{
           backgroundImage:
             "url(https://images.unsplash.com/photo-1541296604437-8cb5efd2da96?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1991&q=80)",
           backgroundPosition: "center center",
@@ -19,6 +19,7 @@ export default function Home() {
         }}
       >
     <HomeSearch/>
+    </div>
     <AboutCards/>
         
     </Container>
