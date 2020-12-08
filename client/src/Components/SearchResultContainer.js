2c052e1a3322606ea2e3100542e8a852ab99547f
Zach Johnson <Zachjdev2121@gmail.com>
2020-12-08 07:15:06 -0500

    very basic styles for search results containers

--- client/src/Components/SearchResultContainer.js
+++ client/src/Components/SearchResultContainer.js
@@ -1,24 +1,51 @@
-import React, { Component } from 'react'
-import {Link} from 'react-router-dom'
-export default class SearchResultContainer extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            picUrl: this.props.pictureUrl
-        }
-        
-    }
-    componentDidMount =() => {
-        this.setState({picUrl:this.state.picUrl || "https://source.unsplash.com/random/?hike"})
-    }
-    render() {
-        let picUrl = this.state.picUrl
-        return (
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
+import useGetPicture from "../hooks/useGetPicture";
+
+export default function SearchResultContainer({ id, name, city, state }) {
+  const [picUrl] = useGetPicture();
+
+  return (
+    <Container
+      className="Search-card"
+      style={{
+        border: "2px solid black",
+        margin: " 1rem auto",
+        maxHeight: "15rem",
+      }}
+    >
+      <Row>
+        <Col>
+          <Image
+            className="Search-card-img"
+            style={{
+              maxHeight: "13rem",
+              margin: "1rem auto",
+            }}
+            src={picUrl}
+            rounded={true}
+            fluid
+          />
+        </Col>
+        <Col xs={9} className="Search-card-info">
+          <div className="Search-card-header">
+            <Link to={`/park/${id}`}>
+              <h2>{name}</h2>
+            </Link>
             <div>
-                <Link to={`/park/${this.props.id}`}><h2>{this.props.name}</h2></Link>
-                <img width={"250px"} src={picUrl}></img>
-                <div><p>{this.props.city} {this.props.state}</p></div>
+              <p>
+                {city} {state}
+              </p>
             </div>
-        )
-    }
+          </div>
+          <div className="Search-card-acc"></div>
+          <div className="Search-card-desc"></div>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
