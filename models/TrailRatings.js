c23e9178b056b6270c68b10d6617d6fdbc4ea1f4
Zach Johnson <Zachjdev2121@gmail.com>
2020-12-16 20:37:59 -0500

    added some random data to the trailRatings table for styling

--- models/TrailRatings.js
+++ models/TrailRatings.js
@@ -1,6 +1,9 @@
+ const { v4: uuidv4 } = require('uuid');
+
 const {DataTypes} = require('sequelize')
 const User = require('./Users')
 const Trail = require('./Trails')
+
 
 module.exports = (sequelize, Sequelize) => {
     const TrailRating = sequelize.define("trailRating", {
@@ -35,5 +38,21 @@ module.exports = (sequelize, Sequelize) => {
 
     }, )
     TrailRating.removeAttribute('id')
+
+    //    for(let i = 0; i < 100; i++) {
+    //         TrailRating.create({
+    //         trailId: 1, 
+    //         userId: uuidv4(),
+    //         startRating: (Math.floor((Math.random() * 5) + 1)),
+    //         petFriendly: Math.random() > .5,
+    //         parking: (['On Trailhead', 'Close', 'Far', 'No Marked Parking'][Math.floor(Math.random() * 4)]),
+    //         wheelchairAcc: Math.random() > .5,
+    //         difficulty: (['Easy', 'Medium', 'Difficult', 'Strenuous'][Math.floor(Math.random() * 4)]),
+    //         goodForGroups: Math.random() > .5,
+    //     }).catch(e => {
+    //         console.log(e)
+    //     })
+    //    }
+    
     return TrailRating
 }
