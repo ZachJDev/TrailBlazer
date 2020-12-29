5aa63e4c6a1c22435cbaafeaa61928073d7146a4
Zach Johnson <Zachjdev2121@gmail.com>
2020-12-28 19:25:59 -0500

    renamed TrailAccessibility folder to be less confusing with the Component of the same name

--- client/src/Components/TrailAccessibility.js
+++ client/src/Components/TrailAccessibility.js
@@ -2,18 +2,18 @@ import React, { useState, useEffect } from "react";
 import useBool from "../hooks/useBool";
 
 import useGetPayload from "../hooks/useGetPayload";
-import Difficulty from "./TrailAccessibility/Difficulty";
-import GoodForGroups from "./TrailAccessibility/GoodForGroups";
-import Parking from "./TrailAccessibility/Parking";
-import PetFriendly from "./TrailAccessibility/PetFriendly";
-import WheelCharAccessible from "./TrailAccessibility/WheelCharAccessible";
+import Difficulty from "./TrailAccessibilityCards/Difficulty";
+import GoodForGroups from "./TrailAccessibilityCards/GoodForGroups";
+import Parking from "./TrailAccessibilityCards/Parking";
+import PetFriendly from "./TrailAccessibilityCards/PetFriendly";
+import WheelCharAccessible from "./TrailAccessibilityCards/WheelCharAccessible";
 
 // Not in love with the '=== "true"' stuff in the actual display. I may play around with that later.
 // Also thinking that doing all this work on the client side is a waste of time and effort. I should try to move 
 // These calculations to the server.
 
 export default function TrailAccessibility({ trailId }) {
-  const MINIMUM_RATINGS = 1;
+  const MINIMUM_RATINGS = 4;
   const [ratings] = useGetPayload(`/ratings/trail/${trailId}`);
   const [ratingsLoaded, updateLoaded] = useBool(false);
   const [difficultyMode, setDifficultyMode] = useState({});
@@ -82,7 +82,7 @@ const getMode = (arr) => {
     const freqs = {};
 
     arr.forEach((el) => {
-      freqs[el.toString()] = ++freqs[el.toString()] || 1;
+      if(el) freqs[el.toString()] = ++freqs[el.toString()] || 1;
     });
 
     // Building the objects for each key is a little dubious, as I will only return one.
