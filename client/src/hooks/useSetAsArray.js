5b157a086019243ee3d37d377b4198311e1b9605
Zach Johnson <Zachjdev2121@gmail.com>
2021-01-01 14:01:39 -0500

    new hook that wraps a Set and exposes it as an array

---
+++ client/src/hooks/useSetAsArray.js
@@ -0,0 +1,23 @@
+import {useState} from 'react'
+
+/**
+ * A hook that returns an array, a function to 
+ * add more values to the array if they are unique,
+ * a function to remove items from the array, and function to check
+ * if the array has an item.
+ * @param {iterable} init 
+ */
+export default function useSetAsArray(init) {
+    const set = new Set(init)
+    const [state, setState] = useState(Array.from(set))
+    const addToSet = (val) => {
+        set.add(val);
+        setState(Array.from(set))
+    }
+    const removeFromSet = (val) => {
+        set.delete(val);
+        setState(Array.from(set))
+    }
+    const has = (val) => set.has(val)
+    return [state, addToSet, removeFromSet, has]
+}
