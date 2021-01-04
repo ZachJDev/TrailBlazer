cbc6cb0fac070c201920c7ab8dffd4983f1d15bc
Zach Johnson <Zachjdev2121@gmail.com>
2021-01-04 11:35:59 -0500

    added conditional for edit / new review routes

--- client/src/Components/InfoContainers/ButtonActionRow.js
+++ client/src/Components/InfoContainers/ButtonActionRow.js
@@ -1,15 +1,19 @@
-import React from "react";
+import React, {useContext} from "react";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Button from "react-bootstrap/Button";
+
+import {UserContext} from '../../contexts/UserContext'
 
 export default function ButtonActionRow({
   handleReview,
   handleShare,
   handleMap,
-  user,
   hasReviewed
 }) {
+
+  const {user} = useContext(UserContext)
+
   return (
     <Row
       style={{
