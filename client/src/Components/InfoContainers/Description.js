5c81cd7d025e6ca810c0d423cdb56dd64c8dc35a
Zach Johnson <Zachjdev2121@gmail.com>
2021-01-04 20:15:21 -0500

    small style shift in trail Desc.

--- client/src/Components/InfoContainers/Description.js
+++ client/src/Components/InfoContainers/Description.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export default function Description({description, name}) {
     return (
@@ -8,11 +9,14 @@ export default function Description({description, name}) {
           borderTop: "1px solid rgb(0,0,0,.1)",
           borderBottom: "1px solid rgb(0,0,0,.1)",
         }}
-      >
-        <h2>About {name}</h2>
+      ><Col xs={12}>
+        <h2>About {name}:</h2>
+        </Col>
+        <Col>
         <p style={{
           textAlign: 'left'
         }}>{description}</p>
+        </Col>
       </Row>
     )
 }
