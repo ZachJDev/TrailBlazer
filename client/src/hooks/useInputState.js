4dc21cd19eb97851fa79781356e6852e343349d2
Zach Johnson <Zachjdev2121@gmail.com>
2021-01-04 19:24:46 -0500

    fixed another bug in my InputState rewrite (will probably need to refactor to just accept strings...

--- client/src/hooks/useInputState.js
+++ client/src/hooks/useInputState.js
@@ -6,9 +6,9 @@ import {useState} from 'react'
  */
 export default function useInputState (initState = "") {
     const [state, setState] = useState(initState);
-    const setInput = (e) => {
-        console.log(e)
-        setState(e?.target?.value || e);
+    const setInput = (e) => { // wrote this originally to accept an event, but wanted to also use strings.
+        const input = typeof e === 'string' ? e : e.target.value
+        setState(input); 
     }
     const clear = () => setState(initState)
     return [state, setInput, clear]
