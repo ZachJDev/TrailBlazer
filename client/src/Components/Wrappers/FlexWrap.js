c8779a8d97c6d19222a750ee6ae95fd636606939
Zach Johnson <Zachjdev2121@gmail.com>
2021-04-23 07:28:07 -0400

    new FlexWrap component

---
+++ client/src/Components/Wrappers/FlexWrap.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function FlexWrapper({children, className, justifyContent="space-between", direction="row",}) {
+   return(
+       <div style={{
+           display: 'flex',
+           flexDirection: direction,
+           justifyContent: justifyContent
+       }} className={className}>
+        {children}
+    </div> )
+}
