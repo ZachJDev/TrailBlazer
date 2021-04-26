1486c63c4f0b121f30d715feeb0dedd04fd85f11
Zach Johnson <Zachjdev2121@gmail.com>
2021-04-26 07:04:36 -0400

    add comment refresh function to review context so it's simpler to call in other places.

--- client/src/contexts/ReviewContext.js
+++ client/src/contexts/ReviewContext.js
@@ -1,11 +1,30 @@
-import React, {createContext, useState} from 'react'
+import React, {createContext, useState, useEffect} from 'react'
+import useGetPayload from '../hooks/useGetPayload';
 export const ReviewContext = createContext()
 
 export function ReviewProvider({children, id}) {
     const [reviewId] = useState(id)
+    const [comments, setComments] = useState([])
+    const [totalComments, setTotal] = useState(0)
+    const [getComments] = useGetPayload(`/comments/byReviewId/${id}`);
+
+    const refreshComments = async () => {
+        const commentRes = await getComments();
+        console.log(commentRes.comments)
+        setComments(commentRes.comments);
+        setTotal(commentRes.total)
+    }
+
+    useEffect(() => {
+        async function refresh() {
+            await refreshComments();
+        }
+            refresh()
+    }, []);
+
 
     return (
-        <ReviewContext.Provider value={{reviewId}}>
+        <ReviewContext.Provider value={{reviewId, comments, totalComments, refreshComments}}>
             {children}
         </ReviewContext.Provider>
     )
