f8e78b3bdc1f397e52e2982765567d6eb98bd411
Zach Johnson <Zachjdev2121@gmail.com>
2021-04-26 10:36:41 -0400

    incorporate a workaround for sequelize's MySql geometry bug: https://github.com/sequelize/sequelize/issues/9786

---
+++ models/sequelizeGeometryHack.js
@@ -0,0 +1,21 @@
+// Sequelize currently has a bug with MySql Geometry types. This fix makes it work.
+// from: https://github.com/sequelize/sequelize/issues/9786
+// Import this file rather than the node_modules one.
+
+const Sequelize = require('sequelize')
+
+const wkx = require('wkx')
+Sequelize.GEOMETRY.prototype._stringify = function _stringify(value, options) {
+    return `ST_GeomFromText(${options.escape(wkx.Geometry.parseGeoJSON(value).toWkt())})`;
+}
+Sequelize.GEOMETRY.prototype._bindParam = function _bindParam(value, options) {
+    return `ST_GeomFromText(${options.bindParam(wkx.Geometry.parseGeoJSON(value).toWkt())})`;
+}
+Sequelize.GEOGRAPHY.prototype._stringify = function _stringify(value, options) {
+    return `ST_GeomFromText(${options.escape(wkx.Geometry.parseGeoJSON(value).toWkt())})`;
+}
+Sequelize.GEOGRAPHY.prototype._bindParam = function _bindParam(value, options) {
+    return `ST_GeomFromText(${options.bindParam(wkx.Geometry.parseGeoJSON(value).toWkt())})`;
+}
+
+module.exports = Sequelize
