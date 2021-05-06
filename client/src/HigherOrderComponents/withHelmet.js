a8f5e30052714151d70dd13184016f8a4e2e267e
Zach Johnson <Zachjdev2121@gmail.com>
2021-05-06 06:14:45 -0400

    Add new HOC to handle most page titles

---
+++ client/src/HigherOrderComponents/withHelmet.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Helmet} from 'react-helmet';
+
+
+const  withHelmet = (helmetOptions) =>
+    (WrappedComponent) =>
+        (ComponentProps) => {
+    return (
+        <React.Fragment>
+            <Helmet>
+                <title>{helmetOptions?.title || 'TrailBlazer | Hike Your Way'}</title>
+            </Helmet>
+            <WrappedComponent {...ComponentProps}/>
+        </React.Fragment>
+    )
+}
+
+export default withHelmet
