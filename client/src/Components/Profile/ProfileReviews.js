abd8deefb93539452461e929d945d53f58e141ec
Zach Johnson <Zachjdev2121@gmail.com>
2021-05-09 19:30:57 -0400

    add some CSS for delete button on profiles. clean up a console.log

--- client/src/Components/Profile/ProfileReviews.js
+++ client/src/Components/Profile/ProfileReviews.js
@@ -1,28 +1,38 @@
-import React, {useEffect, useState} from 'react';
-import useGetPayload from '../../hooks/useGetPayload';
-import TrailReview from '../Reviews/TrailReview';
+import React, { useEffect, useState } from "react";
+import useGetPayload from "../../hooks/useGetPayload";
+import TrailReview from "../Reviews/TrailReview";
 
-import './ProfileReviews.css'
+import "./ProfileReviews.css";
 
-export default function ProfileReviews({userId, username = ''}) {
-    const [reviews, setReviews] = useState(null);
-    const [reviewsRes] = useGetPayload(`/reviews/search/userId=${userId}`);
+export default function ProfileReviews({ userId, username = "" }) {
+  const [reviews, setReviews] = useState(null);
+  const [reviewsRes] = useGetPayload(`/reviews/search/userId=${userId}`);
 
-    useEffect(() => {
-        reviewsRes().then(res => {
-            setReviews(res.reviews);
-        });
-    }, []);
-    console.log(reviews)
-    return (
-        <div className={'profile-reviews'}>
-            {reviews?.length > 0 ?
-                reviews.map(review => {
-                    return (
-                        <TrailReview {...review} reviewId={review.ReviewId} username={username} useComments={false} showTrail={true} showPark={true} park={review.trail.park} trail={review.trail} />
-                    );
-                })
-                : <p>Do Not Have Reviews</p>}
-        </div>
-    );
+  useEffect(() => {
+    reviewsRes().then((res) => {
+      setReviews(res.reviews);
+    });
+  }, []);
+  return (
+    <div className={"profile-reviews"}>
+      {reviews?.length > 0 ? (
+        reviews.map((review) => {
+         return (
+            <TrailReview
+              {...review}
+              reviewId={review.ReviewId}
+              username={username}
+              useComments={false}
+              showTrail={true}
+              showPark={true}
+              park={review.trail.park}
+              trail={review.trail}
+            />
+          );
+        })
+      ) : (
+        <p>Do Not Have Reviews</p>
+      )}
+    </div>
+  );
 }
