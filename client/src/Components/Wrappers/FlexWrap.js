194605d14f925e00c83e367026e458c142d6c2da
Zach Johnson <Zachjdev2121@gmail.com>
2021-05-09 19:22:44 -0400

    Add alignItems prop to the FlexWrapper component

--- client/src/Components/Wrappers/FlexWrap.js
+++ client/src/Components/Wrappers/FlexWrap.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-export default function FlexWrapper({children, className, justifyContent = 'space-between', direction = 'row'}) {
+export default function FlexWrapper({children, className, justifyContent = 'space-between', direction = 'row', alignItems}) {
     return (
         <div style={{
             display: 'flex',
             flexDirection: direction,
             justifyContent: justifyContent,
+            alignItems: alignItems,
         }} className={className}>
             {children}
         </div>);
