12912499e57eb83289828d5bf65189a95c55dcc2
Zach Johnson <Zachjdev2121@gmail.com>
2021-05-09 19:25:49 -0400

    add DeleteUser option to profile for the user themselves and for the admin

--- client/src/Components/Profile/ProfileHeader.js
+++ client/src/Components/Profile/ProfileHeader.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, {useContext} from 'react';
 import ProfileUsername from './ProfileUsername';
 import './ProfileHeader.css'
 import FlexWrapper from '../Wrappers/FlexWrap';
-export default function ProfileHeader({user}) {
+import {Button} from 'react-bootstrap';
+import {UserContext} from '../../contexts/UserContext';
+
+export default function ProfileHeader({userInfo, handleDelete}) {
+    const { user } = useContext(UserContext);
     return (
-        <FlexWrapper justifyContent={'center'} className={"profile-header"}>
+        <React.Fragment>
+        <FlexWrapper direction={'column'} justifyContent={'center'} className={"profile-header"}>
+            <FlexWrapper justifyContent={'center'}>
             <div className={'picture-holder'}></div>
-        <ProfileUsername name={user.username}/>
+        <ProfileUsername name={userInfo.username}/>
+            </FlexWrapper>
+            {(userInfo.userId === user.userId || user.isAdmin) &&
+            <Button onClick={handleDelete} variant={'warning'} className={'button_delete-user'}> Delete User</Button>}
         </FlexWrapper>
+
+        </React.Fragment>
+
     )
 }
