295d24250a74ca2c88cece5e150b06618cc5c1e9
Zach Johnson <Zachjdev2121@gmail.com>
2021-05-09 19:28:45 -0400

    more UI and backend for User Delete

--- client/src/Components/pages/User.js
+++ client/src/Components/pages/User.js
@@ -1,38 +1,59 @@
-import React, {useState, useEffect, useContext} from 'react';
-import useGetPayload from '../../hooks/useGetPayload';
-import ProfileHeader from '../Profile/ProfileHeader';
-import useBool from '../../hooks/useBool';
-import ProfileReviews from '../Profile/ProfileReviews';
-import ProfileBio from '../Profile/ProfileBio'
-import Col from 'react-bootstrap/Col';
-import withHeader from '../../HigherOrderComponents/withHeader';
-import {Helmet} from 'react-helmet';
+import React, { useEffect, useState, useContext } from "react";
+import {Button} from 'react-bootstrap';
+import {faTrash} from '@fortawesome/free-solid-svg-icons/faTrash';
+import { Helmet } from "react-helmet";
 
-export default function User({match}) {
-    const [userInfo, setUserInfo] = useState(null)
-    const [title, setTitle] = useState('TrailBlazer | Hike Your Way')
-    const [isLoaded, flipIsloaded] = useBool(false)
-    const [getUserInfo] = useGetPayload(`/user/${match.params.userId}`)
+import {UserContext} from '../../contexts/UserContext'
+import useGetPayload from "../../hooks/useGetPayload";
+import ProfileHeader from "../Profile/ProfileHeader";
+import useBool from "../../hooks/useBool";
+import ProfileReviews from "../Profile/ProfileReviews";
+import ProfileBio from "../Profile/ProfileBio";
+import Col from "react-bootstrap/Col";
+import withHeader from "../../HigherOrderComponents/withHeader";
 
-    useEffect(() => {
-        getUserInfo().then(info => {
-            setUserInfo(info);
-            setTitle(info.username)
-        })
-    }, [])
-    return (
-        <React.Fragment>
-            <Helmet>
-                <title>{`${title}'s page`}</title>
-            </Helmet>
-            {userInfo !== null ?
-                <Col>
-                    <ProfileHeader user={userInfo}/>
-                    <ProfileBio bio={userInfo.bio}/>
-                    {withHeader(<ProfileReviews userId={userInfo.userId}
-                                                username={userInfo.username}/>, `${userInfo.username}'s reviews:`, 'header')}
-                </Col>
-                : null}
-        </React.Fragment>
-    );
+import useDeleteUser from '../../hooks/Users/useDeleteUser'
+
+
+export default function User({ match, history }) {
+  const [userInfo, setUserInfo] = useState(null);
+  const [title, setTitle] = useState("TrailBlazer | Hike Your Way");
+  const [isLoaded, flipIsloaded] = useBool(false);
+  const [getUserInfo] = useGetPayload(`/user/${match.params.userId}`);
+  const sendDelete = useDeleteUser(userInfo?.userId)
+
+  useEffect(() => {
+    getUserInfo().then((info) => {
+      setUserInfo(info);
+      setTitle(info.username);
+    });
+  }, []);
+
+  const handleDelete = async () => {
+      const deleteRes = await sendDelete();
+      if(deleteRes.success) {history.push('/')}
+      else {console.log(deleteRes.errors[0])}
+  }
+
+  return (
+    <React.Fragment>
+      <Helmet>
+        <title>{`${title}'s Page`}</title>
+      </Helmet>
+      {userInfo !== null ? (
+        <Col>
+          <ProfileHeader userInfo={userInfo} handleDelete={handleDelete} />
+          <ProfileBio bio={userInfo.bio} />
+          {withHeader(
+            <ProfileReviews
+              userId={userInfo.userId}
+              username={userInfo.username}
+            />,
+            `${userInfo.username}'s reviews:`,
+            "header"
+          )}
+        </Col>
+      ) : null}
+    </React.Fragment>
+  );
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,18 @@
 const express = require('express')
-const {getUser, userIsAdmin,} = require('../middleware/middleware')
+const {getUser, authDelete,} = require('../middleware/middleware')
 const userController = require('../controllers/userController')
 const router = express.Router({mergeParams: true})
 
 
 router.get('/:userId', getUser, userController.getUserInfo)
+
+// Delete
+
+router.delete(
+    "/delete/:userId",
+    getUser,
+    authDelete,
+    userController.deleteUser
+);
 
 module.exports = router
