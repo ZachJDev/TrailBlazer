aafa3d530dbf105faec7a2703047d4a60719d7a0
Zach Johnson <Zachjdev2121@gmail.com>
2021-05-12 15:31:30 -0400

    refactor API Routes exports

--- client/src/config/APIRoutes.js
+++ client/src/config/APIRoutes.js
@@ -1,21 +1,10 @@
-// eslint-disable-next-line import/no-anonymous-default-export
 const API_PRE = "/api";
 
-const AUTH_PREFIX = API_PRE + "/auth";
-const PARK_PREFIX = API_PRE + "/park";
-const COMMENT_PREFIX = API_PRE + "/comment";
-const RATING_PREFIX = API_PRE + "/rating";
-const SEARCH_PREFIX = API_PRE + "/search";
-const REVIEW_PREFIX = API_PRE + "/review";
-const TRAIL_PREFIX = API_PRE + "/trail";
-const USER_PREFIX = API_PRE + "/user";
-
-export {
-  PARK_PREFIX,
-  COMMENT_PREFIX,
-  RATING_PREFIX,
-  SEARCH_PREFIX,
-  REVIEW_PREFIX,
-  TRAIL_PREFIX,
-  USER_PREFIX,
-};
+export const AUTH_PREFIX = API_PRE + "/auth";
+export const PARK_PREFIX = API_PRE + "/park";
+export const COMMENT_PREFIX = API_PRE + "/comment";
+export const RATING_PREFIX = API_PRE + "/rating";
+export const SEARCH_PREFIX = API_PRE + "/search";
+export const REVIEW_PREFIX = API_PRE + "/review";
+export const TRAIL_PREFIX = API_PRE + "/trail";
+export const USER_PREFIX = API_PRE + "/user";
