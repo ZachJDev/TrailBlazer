a29b1e51f1b6d2b7c1e202bbd7ee14753f5564f3
Zach Johnson <Zachjdev2121@gmail.com>
2021-05-12 10:34:51 -0400

    Add API routes config

--- client/src/config/APIRoutes.js
+++ client/src/config/APIRoutes.js
@@ -1,14 +1,14 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 const API_PRE = "/api";
 
-const AUTH_PREFIX = API_PRE + "/auth/";
-const PARK_PREFIX = API_PRE + "/park/";
-const COMMENT_PREFIX = API_PRE + "/comment/";
-const RATING_PREFIX = API_PRE + "/rating/";
-const SEARCH_PREFIX = API_PRE + "/search/";
-const REVIEW_PREFIX = API_PRE + "/review/";
-const TRAIL_PREFIX = API_PRE + "/trail/";
-const USER_PREFIX = API_PRE + "/user/";
+const AUTH_PREFIX = API_PRE + "/auth";
+const PARK_PREFIX = API_PRE + "/park";
+const COMMENT_PREFIX = API_PRE + "/comment";
+const RATING_PREFIX = API_PRE + "/rating";
+const SEARCH_PREFIX = API_PRE + "/search";
+const REVIEW_PREFIX = API_PRE + "/review";
+const TRAIL_PREFIX = API_PRE + "/trail";
+const USER_PREFIX = API_PRE + "/user";
 
 export {
   PARK_PREFIX,
