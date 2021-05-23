2c7a61a5b6e43f38b79b51660ea8d96149f6dd8c
Zach Johnson <Zachjdev2121@gmail.com>
2021-05-23 09:53:14 -0400

    refactored Comment Model functions to use async/await

--- models/Comments.js
+++ models/Comments.js
@@ -1,151 +1,116 @@
-const { DataTypes } = require("sequelize");
-const { Op } = require("sequelize");
+const {DataTypes} = require('sequelize');
+const {Op} = require('sequelize');
 const {commentCols, reviewCols, userCols} = require('./ColumnNameConfig');
 
 const {COMMENT_ID, TEXT, PARENT_ID} = commentCols;
 const {REVIEW_ID} = reviewCols;
-const {USER_ID} = userCols;
+const {USER_ID, USERNAME} = userCols;
 
 // Helper Functions
 
 const buildCommentTree = (comments, LoggedInUser) => {
-  // used to count actual length of commentCols section for front-end. (a reply will have a higher key than a sibling)
-  let key = 0;
-  const recursiveBuild = (parent) => {
-    return comments
-      .filter((comment) => comment.parentId === parent)
-      .map(({ text, user, commentId, updatedAt }) => {
-        return {
-          text,
-          user,
-          commentId,
-          updatedAt,
-          key: key++,
-          isEditable: LoggedInUser && LoggedInUser.userId === user.userId,
-          childComments: recursiveBuild(commentId),
-        };
-      });
-  };
-  return recursiveBuild(null);
+    // used to count actual length of commentCols section for front-end. (a reply will have a higher key than a sibling)
+    let key = 0;
+    const recursiveBuild = (parent) => {
+        return comments
+            .filter((comment) => comment.parentId === parent)
+            .map(({text, user, commentId, updatedAt}) => {
+                return {
+                    [TEXT]: text,
+                    [COMMENT_ID]: commentId,
+                    user,
+                    updatedAt,
+                    key: key++,
+                    isEditable: LoggedInUser && LoggedInUser.userId === user.userId,
+                    childComments: recursiveBuild(commentId),
+                };
+            });
+    };
+    return recursiveBuild(null);
 };
 
 const countComments = (comments) => {
-  return comments.reduce((prev, cur) => {
-    return prev + 1 + countComments(cur.childComments);
-  }, 0);
+    return comments.reduce((prev, cur) => {
+        return prev + 1 + countComments(cur.childComments);
+    }, 0);
 };
 
 // Model Export
 
 module.exports = (sequelize) => {
-  // noinspection JSUnresolvedVariable
-  const Comment = sequelize.define("comment", {
-    [USER_ID]: {
-      type: DataTypes.UUID,
-      allowNull: false,
-    },
-    [TEXT]: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    [COMMENT_ID]: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      allowNull: false,
-      primaryKey: true,
-    },
-    [PARENT_ID]: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-    },
-    [REVIEW_ID]: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-  });
-
-  Comment.afterDestroy(async (instance) => {
-    const { commentId } = instance;
-    await Comment.destroy({
-      where: { parentId: commentId },
-      individualHooks: true,
-    });
-    console.log("destroying child comments...");
-  });
-
-  Comment.findByReviewId = (reviewId, user) => {
-    return Comment.findAll({
-      where: { reviewId },
-      include: [
-        {
-          association: Comment.associations.user,
-          attributes: ["username", "userId"],
+    // noinspection JSUnresolvedVariable
+    const Comment = sequelize.define('comment', {
+        [USER_ID]: {
+            type: DataTypes.UUID,
+            allowNull: true,
         },
-      ],
-      attributes: { include: ["text", "commentId", "parentId", "updatedAt"] },
-    }).then((comments) => {
-      return {
-        comments: buildCommentTree(comments, user),
-        total: comments.length,
-      };
-    });
-  };
-  Comment.findByMultipleReviewIds = (reviewIds, user) => {
-    return Comment.findAll({
-      where: { reviewId: { [Op.or]: reviewIds } },
-      include: [
-        {
-          association: Comment.associations.user,
-          attributes: ["username", "userId"],
+        [TEXT]: {
+            type: DataTypes.STRING,
+            allowNull: false,
         },
-      ],
-      attributes: ["text", "commentId", "parentId", "reviewId", "updatedAt"],
-    }).then((comments) => {
-      // noinspection JSUnresolvedVariable
-      return reviewIds.map((revId) => ({
-        reviewId: revId,
-        comments: buildCommentTree(
-          comments.filter((comment) => revId === comment.reviewId),
-          user
-        ),
-      }));
-    });
-  };
-  Comment.addComment = ({ reviewId, parentId, userId, text }) => {
-    console.log("Adding Comment");
-    return Comment.create({ userId, parentId, reviewId, text }).then(
-      (comment) => {
-        return { success: true, comment };
-      }
-    );
-  };
-
-  Comment.updateComment = (commentId, text, userId) => {
-    // noinspection JSUnusedLocalSymbols
-    return Comment.update(
-      { text },
-      {
-        where: {
-          commentId,
-          userId,
+        [COMMENT_ID]: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            allowNull: false,
+            primaryKey: true,
         },
-      }
-    ) // This should stop unauthorized access to other users' comments.
-      .then((comment) => {
-        return { success: true };
-      });
-  };
-
-  Comment.deleteComment = (commentId, userId) => {
-    // Deletes all children as well.
-    console.log("DELETING Comment");
-    // noinspection JSUnusedLocalSymbols
-    return Comment.destroy({
-      where: { commentId, userId },
-      individualHooks: true,
-    }).then((comment) => {
-      return { success: true };
+        [PARENT_ID]: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+        },
+        [REVIEW_ID]: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
     });
-  };
-  return Comment;
-};
+
+    Comment.findByReviewId = async (reviewId, user) => {
+        const foundComments = await Comment.findAll({
+            where: {reviewId},
+            include: [{association: Comment.associations.user, attributes: [USERNAME, USER_ID]},
+            ],
+            attributes: {include: [TEXT, COMMENT_ID, PARENT_ID, REVIEW_ID, 'updatedAt']},
+        });
+        return {
+            comments: buildCommentTree(foundComments, user),
+            total: foundComments.length,
+        };
+    };
+    Comment.findByMultipleReviewIds = async (reviewIds, user) => {
+        const comments = await Comment.findAll({
+            where: {reviewId: {[Op.or]: reviewIds}},
+            include: [{association: Comment.associations.user, attributes: [USER_ID, USERNAME]}],
+            attributes: [TEXT, COMMENT_ID, PARENT_ID, REVIEW_ID, 'updatedAt'],
+        });
+        return reviewIds.map((revId) => ({
+            reviewId: revId,
+            comments: buildCommentTree(
+                comments.filter((comment) => revId === comment[REVIEW_ID]),
+                user,
+            ),
+        }));
+    };
+
+    Comment.addComment = async ({reviewId, parentId, userId, text}) => {
+        const comment = await Comment.create({userId, parentId, reviewId, text});
+        return {success: true, comment: {[COMMENT_ID]: comment[COMMENT_ID]}};
+    };
+
+    Comment.updateComment = async (commentId, text, userId) => {
+        const comment = await Comment.update(
+            {[TEXT]: text},
+            {where: {[COMMENT_ID]: commentId, [USER_ID]: userId}},
+        ); // This should stop unauthorized access to other users' comments.
+        return {success: true, comment: {[COMMENT_ID]: comment[COMMENT_ID]}};
+    };
+
+    Comment.deleteComment = async (commentId, userId) => {
+        // Deletes all children as well.
+        const comment = await Comment.update({[TEXT]: 'Deleted', [USER_ID]: null}, {
+            where: {commentId, userId},
+        });
+        return {success: true, [COMMENT_ID]: comment[COMMENT_ID]};
+    };
+    return Comment;
+}
+;
