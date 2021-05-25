c92a0b88d8bd18ade9b6f6a7682d146994794e24
Zach Johnson <Zachjdev2121@gmail.com>
2021-05-25 07:51:24 -0400

    Add custom method for my errors that simplifies error responses

--- classes/Errors.js
+++ classes/Errors.js
@@ -1,51 +1,83 @@
-class AuthenticationError extends Error {
+class CustomSenderError extends Error {
+    constructor(message) {
+        super(message);
+        Object.setPrototypeOf(this, CustomSenderError.prototype);
+    }
+    sendRes(resObj){
+        return (status, errorMessage) => {
+            return resObj.status(status).json({success: false, errors: [errorMessage]});
+        }
+    }
+    test() {
+        console.log('asdf')
+    }
+}
+
+class AuthenticationError extends CustomSenderError {
   constructor(message) {
     super(message);
     Object.setPrototypeOf(this, AuthenticationError.prototype);
   }
+    sendRes(resObj){
+        return () => {
+            return resObj.status(401).json({success: false, errors: ['User is Not Authenticated']});
+        }
+    }
 }
 
-class InputError extends Error {
+class InputError extends CustomSenderError {
   constructor(message) {
     super(message);
     Object.setPrototypeOf(this, InputError.prototype);
   }
 }
 
-class NotFoundError extends Error {
+class NotFoundError extends CustomSenderError {
   constructor(message) {
     super(message);
     Object.setPrototypeOf(this, NotFoundError.prototype);
   }
+    sendRes(resObj){
+        return (entity = 'entity') => {
+            return resObj.status(404).json({success: false, errors: [`Could not find specified ${entity}`]});
+        }
+    }
 }
 
-class NoContentError extends Error {
+class NoContentError extends CustomSenderError {
   constructor(message) {
     super(message);
     Object.setPrototypeOf(this, NoContentError.prototype);
   }
+
 }
 
-class EntryExistsError extends Error {
+class EntryExistsError extends CustomSenderError {
   constructor(message) {
     super(message);
     Object.setPrototypeOf(this, EntryExistsError.prototype);
   }
 }
 
-class ConflictError extends Error {
+class ConflictError extends CustomSenderError {
   constructor(message) {
     super(message);
     Object.setPrototypeOf(this, ConflictError.prototype);
   }
+    sendRes(resObj){
+        return (entity = 'submission') => {
+            return resObj.status(409).json({success: false, errors: [`Provided ${entity} is not acceptable`]});
+        }
+    }
 }
 
-class QueryError extends Error {
+class QueryError extends CustomSenderError {
   constructor(message) {
     super(message);
     Object.setPrototypeOf(this, QueryError.prototype);
   }
 }
+
 
 module.exports = {
   AuthenticationError,
